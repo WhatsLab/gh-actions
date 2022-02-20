cd4cf3b5904267430b5cfcae45c3e01efcace270
Mohamed Hamza <mohamed.hamza@nana.se>
2022-02-20 15:22:15 +0100

    add build-arg support for docker/build action

--- docker/build/index.js
+++ docker/build/index.js
@@ -5,11 +5,12 @@ const artifact = require('@actions/artifact').create();
 const parser = require('action-input-parser');
 const command = require('../../lib/command');
 
-async function buildImage(dockerfile, context, tags) {
+async function buildImage(dockerfile, context, tags, buildArgs) {
   return command.exec(
     'docker',
     'build',
     ...command.args.map(tags, tag => ['-t ', tag]),
+    ...command.args.map(buildArgs, arg => ['--build-arg ', arg]),
     '-f', dockerfile,
     context,
   );
@@ -29,9 +30,10 @@ async function main() {
     const context = parser.getInput('context');
     const dockerfile = parser.getInput('dockerfile');
     const imageName = parser.getInput('name');
+    const buildArgs = parser.getInput('build-args', { type: 'array' });
     const imagePath = imageName + '.tgz';
 
-    await buildImage(dockerfile, context, [imageName]);
+    await buildImage(dockerfile, context, [imageName], buildArgs);
     await exportImage(imageName, imagePath);
     await artifact.uploadArtifact(imageName, [imagePath], '.');
   } catch (error) {
